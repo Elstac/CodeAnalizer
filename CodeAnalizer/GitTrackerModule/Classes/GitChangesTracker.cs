caf06eeb0915706deaf845753f333f2d43e01970
Jakub <1elstac1@gmail.com>
2018-09-08 19:52:37 +0200

    patrz do dolu

---
+++ CodeAnalizer/GitTrackerModule/Classes/GitChangesTracker.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalizer.GitTrackerModule.Classes
+{
+    public abstract class GitChangesTracker
+    {
+        private List<GitChangesTracker> children;
+
+        public GitChangesTracker()
+        {
+            children = new List<GitChangesTracker>();
+        }
+        protected List<GitChangesTracker> Children { get => children; set => children = value; }
+
+        public virtual Tuple<int, int> ChangedLinesCount()
+        {
+            Tuple<int, int> tmp;
+            int add = 0, del = 0;
+            foreach (var child in children)
+            {
+                tmp = ChangedLinesCount();
+                add += tmp.Item1;
+                del += tmp.Item2;
+            }
+            return new Tuple<int, int>(add, del);
+        }
+
+        public virtual Tuple<int, int> ChangedLinesCount(DateRange dateRnage)
+        {
+            Tuple<int, int> tmp;
+            int add = 0, del = 0;
+            foreach (var child in children)
+            {
+                tmp = ChangedLinesCount(dateRnage);
+                add += tmp.Item1;
+                del += tmp.Item2;
+            }
+            return new Tuple<int, int>(add, del);
+        }
+
+        public virtual int CommitsCount()
+        {
+            int ret = 0;
+            foreach (var child in children)
+            {
+                ret += child.CommitsCount();
+            }
+            return ret;
+        }
+
+        public virtual int CommitsCount(DateRange dateRange)
+        {
+            int ret = 0;
+            foreach (var child in children)
+            {
+                ret += child.CommitsCount(dateRange);
+            }
+            return ret;
+        }
+
+        public virtual List<string> GetChanges()
+        {
+            List<string> ret = new List<string>();
+            foreach (var child in children)
+                ret.AddRange(child.GetChanges());
+            return ret;
+        }
+
+        public virtual List<string> GetChanges(DateRange dateRange)
+        {
+            List<string> ret = new List<string>();
+            foreach (var child in children)
+                ret.AddRange(child.GetChanges(dateRange));
+            return ret;
+        }
+
+        public virtual List<string> MessagesTexts()
+        {
+            List<string> ret = new List<string>();
+            foreach (var child in children)
+                ret.AddRange(child.MessagesTexts());
+            return ret;
+        }
+
+        public virtual List<string> MessagesTexts(DateRange dateRange)
+        {
+            List<string> ret = new List<string>();
+            foreach (var child in children)
+                ret.AddRange(child.MessagesTexts(dateRange));
+            return ret;
+        }
+
+        public void AddChildren(GitChangesTracker gtc)
+        {
+            children.Add(gtc);
+        }
+    }
+}
---
+++ CodeAnalizer/MinorClasses/DateRange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalizer
+{
+    public class DateRange
+    {
+        private DateTime begin;
+        private DateTime end;
+
+        public DateTime Begin { get => begin; set => begin = value; }
+        public DateTime End { get => end; set => end = value; }
+
+        public DateRange(DateTime begin, DateTime end)
+        {
+            this.begin = begin;
+            this.end = begin;
+        }
+
+        public DateRange(DateTime date)
+        {
+            begin = date;
+            end = date;
+        }
+    }
+}

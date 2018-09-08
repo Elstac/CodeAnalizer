0456f1b9b21aa2c7d07cf9076cd5d384b4178249
Jakub <1elstac1@gmail.com>
2018-09-08 17:12:45 +0200

    Dostosowanie IGitChangeTrackera do DateRangea

--- CodeAnalizer/ProjectAnalizerModule/Classes/MinorClasses/DateRange.cs
+++ CodeAnalizer/ProjectAnalizerModule/Classes/MinorClasses/DateRange.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeAnalizer.ProjectAnalizerModule.Classes.MinorClasses
+namespace CodeAnalizer
 {
     class DateRange
     {
--- CodeAnalizer/ProjectAnalizerModule/Interfaces/IGitChangeTracker.cs
+++ CodeAnalizer/ProjectAnalizerModule/Interfaces/IGitChangeTracker.cs
@@ -6,21 +6,15 @@ namespace CodeAnalizer
     interface IGitChangesTracker
     {
         Tuple<int,int> ChangedLinesCount();
-        Tuple<int, int> ChangedLinesCount(DateTime date);
-        Tuple<int, int> ChangedLinesCount(DateTime from, DateTime to);
+        Tuple<int, int> ChangedLinesCount(DateRange dateRnage);
 
         List<string> MessagesTexts();
-        List<string> MessagesTexts(DateTime date);
-        List<string> MessagesTexts(DateTime from, DateTime to);
+        List<string> MessagesTexts(DateRange dateRange);
 
         int CommitsCount();
-        int CommitsCount(DateTime date);
-        int CommitsCount(DateTime from, DateTime to);
-        int CommitsCount(string authorName);
+        int CommitsCount(DateRange dateRange);
 
         List<string> GetChanges();
-        List<string> GetChanges(DateTime date);
-        List<string> GetChanges(DateTime from, DateTime to);
-        List<string> GetChanges(string author);
+        List<string> GetChanges(DateRange dateRange);
     }
 }

e2dfba4d6563758f5bb9ce05497a6f39ab69b05b
Jakub <1elstac1@gmail.com>
2018-09-09 09:58:26 +0200

    Dodanie ContributorsFindera do doczytywania wszystich osob z repozytorium

---
+++ CodeAnalizer/GitTrackerModule/Classes/ContributorsFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+namespace CodeAnalizer.GitTrackerModule.Classes
+{
+    class ContributorsFinder
+    {
+
+        public static List<string> FindContributors(Repository repository)
+        {
+            List<string> ret = new List<string>();
+
+            Commit[] commits = repository.Commits.ToArray();
+            foreach (var commit in commits)
+            {
+                if (ret.Contains(commit.Author.Name))
+                    continue;
+
+                ret.Add(commit.Author.Name);
+            }
+            return ret;
+        }
+    }
+}
---
+++ CodeAnalizer/GitTrackerModule/Classes/RepoTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LibGit2Sharp;
+namespace CodeAnalizer.GitTrackerModule.Classes
+{
+    public class RepoTracker: GitChangesTracker
+    {
+        public RepoTracker(string pathToRepo)
+        {
+
+        }
+    }
+}
